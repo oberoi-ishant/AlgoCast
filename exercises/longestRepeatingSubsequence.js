b655df0d74deb7842e57cbbe404f2b913b6b1476
ishant.oberoi <ishant.oberoi@nutanix.com>
2020-07-26 18:20:14 +0530

    More questions

--- exercises/longestRepeatingSubsequence.js
+++ exercises/longestRepeatingSubsequence.js
@@ -1,18 +1,17 @@
+// https://www.geeksforgeeks.org/longest-repeated-subsequence/
+// Given a string, print the longest repeating subsequence such that the two
+// subsequence don’t have same string character at same position, i.e.,
+// any i’th character in the two subsequences shouldn’t have the same index in the
+// original string.
 // Given a string find the longest repeating subsequence.
-// s1='aabebcdd'
+// s1='AABABCD'
 // Now here we see, a subsequence like abeb, but this is not repeating in the string.
 // There are many subsequences in this string.
-// if we consider, abd as susequence and remove it from string.
-// The remaing string is aebcdd. Now we can again get subsequence abd.
-// Now remaining string id ecd. So we see abd is a subsequence we can get 2 time.
-// We can also consider just a or b as subsequence and they repeat. But we
-// need to longest repeating subsequence.
-// Remember every char in the string can be used only once.
+// Here if we take subsequence AAB.
+// We can first create it using A(0)A(1)B(2) ... from indexes 0,1,2
+// We can again create it using A(1)A(3)B(4) ... from indexes 1,3,4
+// So ith char in both substring is not haveing same index in origin string.
 
-
-// So what we did was after we got abd as substring we checked the remaing
-// string for another subseq of abd. Meaning we checked a b d are present
-// at some other index also in the string or not.
 
 // We know two find LCS we need two strings.
 // If we have one string, we sometimes get second string from first by applying
@@ -42,11 +41,34 @@ function LCS(s1, s2, n, m) {
   // If we do not check this n!==m, we are basically comparing same index
   // and our answer for LCS will be length of the complete string s1 itself.
   // As s2 = s1.
-  // So idea is to check if we can get chars at different indexes and if they are equal
-  // are we able to get a longest subsequence.
+  // So idea is to check if we can get chars at different indexes
+  // and create the same longest subsequence.
   if(s1[n-1] == s2[m-1] && n !== m) {
     return 1+LCS(s1, s2, n-1 ,m-1);
   } else {
     return Math.max(LCS(s1,s2, n, m-1), LCS(s1,s2, n-1, m));
   }
 }
+
+
+// Whoever has confusion in the following inputs :
+
+
+// AABABCD
+// AXXXY.  etc.
+// Here is the explanation:
+// Read this: Given a string, print the longest repeating subsequence such that the two subsequences
+// don’t have same string character at same position
+
+
+
+
+// The same Position is the twist here: A X X X Y ==> You can take X X [index0 and index1]
+// and XX [index 1 and index2]
+
+
+
+
+// X of index1 is used in both but its position in both substrings is different.
+// In the first subsequence, it comes at 1st index whereas in
+// the second subsequence comes at the 0th index.
